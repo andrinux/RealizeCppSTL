287aea7d2128d2b9117d8e0e5d79d79733f909ab
ZhangVincent <xuebinzhang.whu@gmail.com>
2015-04-09 17:39:22 -0400

    HashSet Implementation: update some functions

--- ExtHashSet.h
+++ ExtHashSet.h
@@ -1,6 +1,8 @@
 #ifndef Ext_Hash_Set
 #define Ext_Hash_Set
 
+#define INIT_TABLE_SIZE 8
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -50,15 +52,15 @@ public:
 		//The comparison operator must account fo the list iterators
 		//being unassigned at the end.
 		friend bool operator==(const iterator &lft, const iterator &rgt){
-			return lft.m_hs == rgt.m_hs && lft.m_index = rgt.m_index &&
-				(lft.m_index == -1 || lft.m_list_itr == rgt.m_list_itr;)
+			return lft.m_hs == rgt.m_hs && lft.m_index == rgt.m_index &&
+				(lft.m_index == -1 || lft.m_list_itr == rgt.m_list_itr);
 		}
 		friend bool operator!=(const iterator&lft, const iterator &rgt){
 			return lft.m_hs != rgt.m_hs || lft.m_index != rgt.m_index ||
 				(lft.m_index != -1 && lft.m_list_itr != rgt.m_list_itr);
 		}
 		//increment and decrement
-		iterator & operator++(){
+		iterator & operator++(){ //XZ: p++ or ++p?
 			this->next();
 			return *this;
 		}
@@ -101,7 +103,7 @@ public:
 				m_list_itr--;
 			else{
 				//If not, back down the table to find a previous non-empty list
-				for (--m_index; m_index > 0 && m_hs->m_table[m_index].empty(); --m_index) {}
+				for (--m_index; m_index > 0 && m_hs->m_table[m_index].empty(); --m_index) {} //XZ: empty condition?
 				//Go to the last entry in the found list
 				m_list_itr = m_hs->m_table[m_index].begin();
 				hash_list_itr p = m_list_itr;
@@ -121,7 +123,7 @@ public:
 	//====================HashSet Implementations================
 	//Constructor of the table accepts the size of the table. Default
 	//Constructor for the hash function object is used
-	ExtHashSet(unsigned int init_size = 10) :m_table(init_size), m_size(0) {}
+	ExtHashSet(unsigned int init_size = INIT_TABLE_SIZE) :m_table(init_size), m_size(0) {}
 	ExtHashSet(const ExtHashSet<KeyType, HashFunc>& old) :
 		m_table(old.m_table), m_size(old.m_size) {} //XZ: this is only pointer -redirect, not copy???
 	~ExtHashSet() {}
@@ -140,41 +142,98 @@ public:
 		const float LOAD_FRACTION_FOR_RESIZE = 1.25;
 		if (m_size >= LOAD_FRACTION_FOR_RESIZE*m_table.size())
 			this->resize_table(2 * m_table.size() + 1);
+		bool success = false;
+		//With help of T.L.
+		unsigned int hash_value = m_hash(key);
+		unsigned int index = hash_value%m_table.size();
+		hash_list_itr p = std::find(m_table[index].begin(), m_table[index].end(), key);  //XZ: why here needs std::
+		if (p == m_table[index].end()){
+			//didn't find, the do the insert
+			m_table[index].push_back(key);
+			success = true;
+			m_size++;
+		}
+		return std::make_pair( iterator(this, index, p), success); 
+		//XZ: p is pointed to the end of previous vector, 
+		//which is the loc where new entry is inserted
 	}
 
 	//Find the location of one key
 	iterator find(const KeyType& key){
-
+		unsigned int hash_value = m_hash(key);
+		unsigned int index = hash_value % m_table.size();
+		hash_list_itr p = std::find(m_table[index].begin(), m_table[index].end(), key);
+		if (p == m_table[index].end())
+			return this->end();  //XZ: why return end here?
+		else
+			return iterator(this, index, p);
 	}
 
 	//Erase One key
 	int erase(const KeyType& key){
-
+		//XZ: need to find the key first and then erase
+		iterator p = find(key);
+		if (p == end()) //Call end() directly?
+			return 0;
+		else {
+			erase(p);
+			return 1;
+		}
 	}
 	//An overloaded version of erase at iterator
-	int erase(iterator p){
-		
+	void erase(iterator p){
+		m_table[p.m_index].erase(p.m_list_itr); //XZ: call vector's erase function
+
 	}
 
 	//Find the first entry in the table and create an iterator
 	iterator begin(){
+		//version1: 
+		//iterator p(this);
+		//p.m_index = 0;
+		//p.m_list_itr = m_hs->m_table[m_index].begin();
+		//return iterator(p);
+		//XZ: version 1's problem should be that m_index==0, maybe this list is empty
+
+		unsigned int index = 0;
+		while (m_table[index].size() == 0){
+			index++;
+		}
+		hash_list_itr p = m_table[index].begin();
+		return iterator(this, index, p);
 
 	}
 
 	//Create and end iterator
 	iterator end(){
-
+		iterator p(this); //XZ: where the "this" pointing to?
+		p.m_index = -1;  //XZ: ?? so simple?
+		return p;
 	}
 
 	//A public print utility
 	void print(std::ostream & ostr){
-
+		for (unsigned int i = 0; i<m_table.size(); ++i) {
+			ostr << i << ": ";
+			for (hash_list_itr p = m_table[i].begin(); p != m_table[i].end(); ++p)
+				ostr << ' ' << *p;
+			ostr << std::endl;
+		}
+		ostr << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
 	}
 
 
 	//=====Some private interface of HashSet========
 	void resize_table(unsigned int new_size){
-
+		//A simple way is to create a large new table and copy the whole old table
+		std::vector<std::list<KeyType> > temp_table(new_size);
+		iterator p = this->begin();
+		for (; p != this->end(); ++p){
+			unsigned int hash_value = m_hash(*p);
+			unsigned int index = hash_value % new_size;
+			temp_table[index].push_back(*p);
+		}
+		m_table = temp_table;
 	}
 };
 
