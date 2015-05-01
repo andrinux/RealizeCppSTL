9778c533b170eb0fd1dccae56fc880ebaf68d420
Andrinux Loid <andrinux.loid@gmail.com>
2015-04-30 22:59:06 -0400

    Implementation of String

---
+++ ExtString.h
@@ -0,0 +1,14 @@
+//Implementation of String
+#ifndef _EXT_STRING_H
+#define _EXT_STRING_H
+
+#include <cstring>
+
+class ExtString{
+public:
+	typedef char value_type;
+	typedef char * iterator;
+	
+};
+
+#endif
